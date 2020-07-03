1daf4c789641c904681ecc7879a1181307c1423c
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2020-07-03 14:51:02 -0400

    Added some templating code to weather-app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,7 +1,36 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+
+app.set('view engine', 'hbs')
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        author: 'Rupen Bandyopadhyay'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        author: 'Rupen Bandyopadhyay'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: 'Use this guide to navigate this application and use it effectively'
+    })
+})
+
+// The below no longer works as it is going to look up the index.html from the public directory
+/*
 app.get('', (req, res) => {
     res.send('<h1>Weather</h1>')
 })
@@ -16,11 +45,12 @@ app.get('/help', (req, res) => {
 app.get('/about', (req, res) => {
     res.send('<b><u>About the weather app</u></b>')
 })
+*/
 
 app.get('/weather', (req, res) => {
     res.send({
         location: 'Gaithersburg, MD',
-        forecast: 'Sunny day'
+        forecast: 'Sunny Day'
     })
 })
 
